89d3c2b05fb193d97bef8c39d3c35eb7ce1ac310
SamahAyman <43833050+SamahAyman@users.noreply.github.com>
2019-12-10 21:55:07 +0200

    Update simulator.cpp

--- simulator.cpp
+++ simulator.cpp
@@ -10,56 +10,21 @@
 using namespace std;
 
 int regs[8] = { 0 };   //array of registers 
-int pc = 0x0;  //location 0x00000000 in the main memory
-int clk = 1,
-//loading instructions from file
-void File_Instructions(vector<string> inst_str, int Number_of_instructions)
+int pc = 0x0;          //location 0x00000000 in the main memory
+int clk = 1;
+int Memory[64 * 1024] = { 0 }; //64 * 1024 bytes * 2 bytes = 128KB, but word addressable so 64 * 1024 is used 
+void RS_TB_initialize(reservation_station_table RS_TB[15]);
+void File_Instructions(vector <string> inst_str);
+void Manual_Instructions(vector <string> inst_str, int inst_count);
+
+int main() 
 {
-	string filename;
-	bool found = false;
-	while (!found) //the loop is repeated till the file opens
-	{
-		cout << "Enter the name of the instruction file " << endl;
-		cin >> filename;
-		ifstream file(filename);
 
-		if (file.is_open())
-		{
-			int i = 0;
-			while ((i < Number_of_instructions) && (file))
-			{
-				getline(file, inst_str[i]);
-				i++;
-			}
-			found = true;
-		}
-		else
-		{
-			cout << "File Not Found!" << endl;
-			found = false;
-		}
-	}
-
-}
-
-//Manually enter instructions
-void Manual_Instructions(vector<string> inst_str, int Number_of_instructions)
-{
-	cout << "Enter the instructions";
-	string in;
-	for (short int i = 0; (i < Number_of_instructions); i++)
-	{
-		cout << "i:" << (i + 1);
-		cin >> in;
-		inst_str[i] = in;
-	}
-}
-int main() {
-	int Memory[64 * 1024] = { 0 }; //64 * 1024 bytes * 2 bytes = 128KB, but word addressable so 64 * 1024 is used (char or int??)
-	int Mem_Location;
+	int Mem_Location=0;
 	int Mem_Val;
-	int inst_count; // to be read from the user
+	int inst_count; 
 	int User_choice;
+
 	//updating the memory 
 	while (Mem_Location >= 0)//terminate using any number less than 0 
 	{
@@ -70,17 +35,20 @@ int main() {
 		if (Mem_Location >= 0)
 			Memory[Mem_Location] = Mem_Val;
 	}
-	
-	
+
 	cout << " Please choose (0) to read from a file or (1) to enter the instructions manually" << endl;
 	cin >> User_choice;
-	vector<string> instruction_line(inst_count);
-	if (User_choice == 1) {
+
+	
+	vector <string> instruction_lines;
+
+	if (User_choice == 1) 
+	{
 		cout << "Enter number of instrutions: ";
 		cin >> inst_count;
-		Manual_Instructions(instruction_line, inst_count);
+		Manual_Instructions(instruction_lines, inst_count);
 	}
-	else File_Instructions(instruction_line, inst_count);
+	else File_Instructions(instruction_lines);
 
 
 
@@ -88,85 +56,83 @@ int main() {
 	exit(0);
 }
 
-void RS_TB_initialize(RS_Table RS_TB[15])
+void RS_TB_initialize(reservation_station_table RS_TB[15])
 {
-	
+
 	//For load
 	int j = 0;
-	for (int i= 0; i < LW[0]; i++)
+	for (int i = 0; i < LW_RS; i++)
 	{
 		RS_TB[i].FU_name = "LOAD" + to_string(j++);
-		RS_TB[i].Busy = 0; 
+		RS_TB[i].busy = 0;
 		RS_TB[i].op = load;
 		RS_TB[i].dest = 0;
 		RS_TB[i].A = 0;
 	}
 	//For Store
 	j = 0;
-	for (int i=0; i < SW[0] ; i++)
+	for (int i = 0; i < SW_RS; i++)
 	{
 		RS_TB[i].FU_name = "STORE" + to_string(j++);
-		RS_TB[i].Busy = 0;
+		RS_TB[i].busy = 0;
 		RS_TB[i].op = store;
 		RS_TB[i].dest = 0;
 		RS_TB[i].A = 0;
 	}
 	//For Jump
 	j = 0;
-	for (int i=0 ; i < JMP[0]; i++)
+	for (int i = 0; i < JMP_RS; i++)
 	{
 		RS_TB[i].FU_name = "JUMP" + to_string(j++);
-		RS_TB[i].Busy = 0;
+		RS_TB[i].busy = 0;
 		RS_TB[i].op = add;
 		RS_TB[i].dest = 0;
 		RS_TB[i].A = 0;
 	}
 	//For Branch
 	j = 0;
-	for (int i=0; i < BEQ[0]; i++)
+	for (int i = 0; i < BEQ_RS; i++)
 	{
 		RS_TB[i].FU_name = "BRANCH" + to_string(j++);
-		RS_TB[i].Busy = 0;
+		RS_TB[i].busy = 0;
 		RS_TB[i].op = add;
 		RS_TB[i].dest = 0;
 		RS_TB[i].A = 0;
 	}
-	//For Arith
-	j = 0;
-	for (int i=0; i < ; i++)
+}
+
+// loading instructions from file
+void File_Instructions(vector <string> inst_str)
+{
+	string filename;
+
+	cout << "Enter the name of the instruction file " << endl;
+	cin >> filename;
+
+	ifstream file(filename);
+
+	if (file.is_open())
 	{
-		res_tb[i].Name = "ADD" + to_string(j + 1);
-		res_tb[i].Busy = 0; //Initially Mark as Free
-		res_tb[i].Address = 0;
-		res_tb[i].OpCycle = 0;
-		res_tb[i].temp_result = 0;
-		res_tb[i].Initial_Cycle = 0;
-		j++;
+		int i = 0;
+		while (!file.eof())
+		{
+			getline(file, inst_str[i]);
+			i++;
+		}
 	}
-	//For NAND
-	j = 0;
-	NCounter = i;
-	for (i; i < (NCounter + NAND_RS); i++)
+	else
+		cout << "File Not Found!" << endl;
+
+}
+
+//Manually enter instructions
+void Manual_Instructions(vector <string> inst_str, int inst_count)
+{
+	cout << "Enter the instructions";
+	for (int i = 0; i < inst_count; i++)
 	{
-		res_tb[i].Name = "NAND" + to_string(j + 1);
-		res_tb[i].Busy = 0; //Initially Mark as Free
-		res_tb[i].Address = 0;
-		res_tb[i].OpCycle = 0;
-		res_tb[i].temp_result = 0;
-		res_tb[i].Initial_Cycle = 0;
-		j++;
-	}
-	//For Multiply
-	j = 0;
-	NCounter = i;
-	for (i; i < (NCounter + MULT_RS); i++)
-	{
-		res_tb[i].Name = "MULT" + to_string(j + 1);
-		res_tb[i].Busy = 0; //Initially Mark as Free
-		res_tb[i].Address = 0;
-		res_tb[i].OpCycle = 0;
-		res_tb[i].temp_result = 0;
-		res_tb[i].Initial_Cycle = 0;
-		j++;
+		cout << "i" << (i + 1) << ": ";
+		getline(cin, inst_str[i]);
 	}
 }
+
